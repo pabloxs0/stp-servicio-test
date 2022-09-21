60dc8e75da21b144211d2eb5a956f45d1496e460
Pablo <pabloxs@msn.com>
2022-09-21 11:25:15 -0500

    First

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@
 * sudo git pull
 *
 * RESETEAR SERVICIO:
-* pm2 restart stp-app
+* pm2 restart index
 *
  */
 const express = require('express');
@@ -54,7 +54,7 @@ const morgan = require('morgan');
 
 //configuraciones
 //app.set('port', process.env.PORT || 7024);
-app.set('port', 80);
+app.set('port', 7025);
 app.set('json spaces', 2);
 
 //middleware
@@ -69,5 +69,5 @@ app.use(require('./routes_stp_ws/stp_ws'));
 app.use(require('./routes/pruebas'));
 //start server
 app.listen(app.get('port'),()=> {
-   console.log(`Server on port ${7024}`);//backtick (alt+96)
+   console.log(`Server on port ${7025}`);//backtick (alt+96)
 });
