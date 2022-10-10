2d047541cdcae80e0ae4de40ed6fd2c4294d05ab
Pablo <pabloxs@msn.com>
2022-10-10 17:19:03 -0500

    First

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 //routes
 app.use(require('./routes/cobranza'));
 app.use(require('./routes/cambio_estado'));
-app.use(require('./routes_stp_ws/stp_ws'));
+//app.use(require('./routes_stp_ws/stp_ws'));
 app.use(require('./routes/pruebas'));
 //start server
 app.listen(app.get('port'),()=> {
