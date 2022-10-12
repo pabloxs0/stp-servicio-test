529cdf18ebeac164cab393dc506289d933e129d0
Pablo <pabloxs@msn.com>
2022-10-12 11:19:28 -0500

    First

--- src/routes_stp_ws/stp_ws.js
+++ src/routes_stp_ws/stp_ws.js
@@ -3,7 +3,7 @@ const router = Router();
 
 router.post('/ws_test', (req, res) => {
     //https://efws-dev.stpmex.com/efws/API/conciliacion
-    let responseBody = JSON.parse(this.err._body);
+//    let responseBody = JSON.parse(this.err._body);
     let json_test = "{\"mensaje\": \"[{\"idEF\":323297083,\"claveRastreo\":\"BNET01002210070039492186\"\"}"
     res.json(json_test);
 })
